c7e6db99b68752855bb34bc3bdb3c681e29982c9
hadnle-less <salaev-08@mail.ru>
2024-06-03 16:45:05 +0300

    lab

---
+++ include/tasks.h
@@ -0,0 +1,8 @@
+// Copyright 2024 Salaev Vladislav
+#ifndef INCLUDE_TASKS_H
+#define INCLUDE_TASKS_H
+
+double calculateGapAfterAddingRope(double initialRadius, double addedLength);
+double calculateCostForPool(double poolRadius, double pathWidth, double concreteCost, double fenceCost);
+
+#endif // INCLUDE_TASKS_H
